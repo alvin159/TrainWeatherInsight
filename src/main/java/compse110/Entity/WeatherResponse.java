17ffdde3c9bdb991a5a483410b9545596dc3d64d
Yue <yue.zhu@tuni.fi>
2024-10-03 19:10:43 +0300

    Implement WeatherResponse Payload

--- src/main/java/compse110/Entity/WeatherResponse.java
+++ src/main/java/compse110/Entity/WeatherResponse.java
@@ -1,4 +1,38 @@
 package compse110.Entity;
 
 public class WeatherResponse {
+    private double temp;
+    private String description;
+
+    public WeatherResponse(double temp, String description) {
+        this.temp = temp;
+        this.description = description;
+    }
+
+    public WeatherResponse() {}
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // 重写 toString 方法
+    @Override
+    public String toString() {
+        return "WeatherResponse{" +
+                "temp=" + temp +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
