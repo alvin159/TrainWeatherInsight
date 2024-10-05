cd90aaf50e04d8dd2a97ca2ca37bf51cc5a90963
Juhana Kivelä <juhana.kivela@tuni.fi>
2024-10-05 12:41:32 +0300

    Move AbbrevConvertTest to its correct location. Don't test it using message broker.

---
+++ src/test/java/backend/SearhStationComponent/AbbrevConvertTest.java
@@ -0,0 +1,10 @@
+package backend.SearhStationComponent;
+
+import compse110.backend.SearhStationComponent.AbbrevConvert;
+
+public class AbbrevConvertTest {
+
+    public static void main(String[] args) {
+
+    }
+}
