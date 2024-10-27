fa0e8fe46fd712b0373af43e94c72257f2f27fb2
Juhana Kivelä <juhana.kivela@tuni.fi>
2024-10-27 14:22:25 +0200

    Disable TrainComponentTest as the implementation changed

--- src/test/java/backend/TrainComponentTest.java
+++ src/test/java/backend/TrainComponentTest.java
@@ -1,21 +1,4 @@
 package backend;
 
-
-import compse110.Entity.TrainData;
-import compse110.backend.TrainComponent;
-
-import java.util.Date;
-import java.util.List;
-
 public class TrainComponentTest {
-
-    public static void main(String[] args) {
-        TrainComponent trainComponent = new TrainComponent();
-        List<TrainData> trainData = trainComponent.getTrainData(new Date(), "HKI");
-        List<TrainData> trainData2 = trainComponent.getTrainData(new Date(),"HKI", "TPE");
-        System.out.println(trainData);
-        System.out.println("\n\n");
-        System.out.println("HKI size: " + trainData.size());
-        System.out.println("HKI to TPE size: " + trainData2.size());
-    }
 }
